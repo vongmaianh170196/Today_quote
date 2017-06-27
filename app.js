20e0841cfde0d8e98a10b32084f31efb2a782776
Anh <anh.vo@student.lamk.fi>
2017-06-27 14:34:14 +0300

    Color changes done

--- app.js
+++ app.js
@@ -5,13 +5,39 @@ var $newquote = $('.new-quote');
 /*Color array*/
 var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 /*random color*/
-var color = Math.floor(Math.random() * colors.length);
 var quot;
+function getQuote(){
+    //e.preventDefault();
+    $.ajax({
+        success : function(r){
+            var color = Math.floor(Math.random() * colors.length);
+
+            $("html body").animate({
+        backgroundColor:colors[color],
+        color:colors[color]
+ },1000);
+ $(".button").animate({
+     backgroundColor:colors[color]
+        
+
+ },1000);
+        }
+    });
+
+}
+
+
+
 $(document).ready(function(){
-$newquote.on("click", function(e){
+    
+$newquote.on('click', getQuote);
+/*{
     e.preventDefault();
     var linkquote = "https://en.wikipedia.org/w/api.php";
-    $.getJSON(linkquote,function(data){
+    var quotedata =  {
+                    format: 'json'
+                  };
+    $.getJSON(linkquote,quotedata, function(data){
         $.each(data, function(quotes){
             quot += '<p>' + quotes.quotes + '</p>';
         })
@@ -20,10 +46,12 @@ $newquote.on("click", function(e){
 
 
 
-})
+
+}*/
 
     /*changing color function
-$newquote.click(function(){
+$newquote.click(function(e){
+    e.preventDefault();
     $("html body").animate({
         backgroundColor:colors[color],
         color:colors[color],
