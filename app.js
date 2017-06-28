6448d22593b50a2e630fb27e9835dca2cd918d8b
Anh <anh.vo@student.lamk.fi>
2017-06-29 01:29:44 +0300

    Added json file for data

--- app.js
+++ app.js
@@ -5,22 +5,33 @@ var $newquote = $('.new-quote');
 /*Color array*/
 var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 /*random color*/
-var quot;
+
+
+var authors = $('.author');
 function getQuote(){
     //e.preventDefault();
     $.ajax({
-        success : function(r){
+       /*url: '../data/quote.json',*/
+        success : function(data){
+           // var randomQuot = Math.floor(Math.random() * quotes.length);
+           
+           /*$.each(data, function(index, quotes){
+            var newText = '<p>' + data.quotes.quot + '</p>';
+           newText += '<p class="author">' + data.quotes.author + '</p>';
+        });
+        $('.quote').html(newText);
+        /*******Change color after every click*/
             var color = Math.floor(Math.random() * colors.length);
-
             $("html body").animate({
-        backgroundColor:colors[color],
-        color:colors[color]
- },1000);
- $(".button").animate({
-     backgroundColor:colors[color]
+                 backgroundColor:colors[color],
+                 color:colors[color]
+            },1000);
+            $(".button").animate({
+                 backgroundColor:colors[color]
         
 
- },1000);
+            },1000);
+        /*********** End Change color after every click*/
         }
     });
 
